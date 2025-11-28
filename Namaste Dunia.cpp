b15be823a228d889501bf744234d7a04f3fa7912
patelsumit77069-rgb <patelsumit77069@gmail.com>
2025-11-28 12:38:33 +0530

    Add files via upload

---
+++ Namaste Dunia.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  cout <<"Namaste Dunia" << endl;
+ /*/ int a=5;
+  cout << a << endl;
+  char c='a';
+  cout << c << endl;
+  bool bl=true;
+  cout << bl << endl;
+  float fl=1.5;
+  cout << fl << endl;
+  double dl=1.56;
+  cout << dl << endl;
+  int size=sizeof(a);
+  cout << size << endl;
+  int b='c';
+  cout << b <<endl;
+  char cd=5;
+  cout << cd <<endl;
+  unsigned int e=-5;
+  cout << e << endl;
+  float d=2.0/5;
+  cout << d <<endl;
+ */ int f=5;
+  int r=6;
+  bool first=(f==r);
+  cout << first<<endl;
+  bool second=(f<r);
+  cout << second <<endl;
+  bool third=(f>r);
+  cout << third <<endl; 
+  bool fourth=(f>=r);
+  cout << fourth <<endl;
+  bool fifth=(f<=r);
+  cout << fifth <<endl;
+  
+
+}
